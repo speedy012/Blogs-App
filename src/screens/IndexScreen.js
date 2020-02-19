ca8afa147a0169e8fa27768aa78676bf3fbbed7b
Herman Rivas <hrivas5726@aol.com>
2020-02-18 21:37:17 -0500

    added create component

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -16,12 +16,12 @@ const IndexScreen = ({ navigation }) => {
         renderItem={({ item }) => {
           return (
             <TouchableOpacity onPress={() => navigation.navigate('Show',{ id: item.id })}>
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title} - {item.id}</Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                <Feather style={styles.icon} name="trash"/>
-              </TouchableOpacity>
-            </View>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash"/>
+                </TouchableOpacity>
+              </View>
             </TouchableOpacity>
           )
         }}
@@ -29,6 +29,17 @@ const IndexScreen = ({ navigation }) => {
     </View>
 
   )
+}
+
+
+IndexScreen.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" style={styles.plus} />
+      </TouchableOpacity>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -39,16 +50,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     borderTopWidth: 1,
     borderColor: 'gray'
-
   },
   title: {
     fontSize: 18
   },
   icon: {
     fontSize: 24
+  },
+  plus: {
+    fontSize: 30
   }
-
-
 })
 
 export default IndexScreen
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
 
 const ShowScreen = ({ navigation }) => {
-  
+  const { state } = useContext(Context)
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
 
   return (
     <View>
-      <Text> Show Screen </Text>
+      <Text> {blogPost.title} </Text>
     </View>
   )
 }
