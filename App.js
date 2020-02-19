70d896a23b122f78fa4ffee10f2b898ca545f664
Herman Rivas <hrivas5726@aol.com>
2020-02-18 19:53:24 -0500

    added components

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import IndexScreen from './src/screens/IndexScreen'
+import { Provider } from './src/context/BlogContext'
 
-export default function App() {
+const navigator = createStackNavigator ({
+  Index: IndexScreen
+},
+{
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
+})
+
+const App = createAppContainer(navigator)
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Blog!</Text>
-    </View>
-  );
+    <Provider>
+      <App />
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
