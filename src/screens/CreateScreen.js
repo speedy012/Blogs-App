3509c9d8329e759cc10a86ad60e63df08baff2c7
Herman Rivas <hrivas5726@aol.com>
2020-02-20 21:02:03 -0500

    added edit comp

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -14,7 +14,13 @@ const CreateScreen = ({ navigation }) => {
       <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
       <Text style={styles.label}> Enter Content: </Text>
       <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
-      <Button onPress={()=> addBlogPost(title, content)} title="Add Blog Post"/>
+      <Button
+        onPress={()=> {
+          addBlogPost(title, content, () => {
+            navigation.navigate('Index')
+        })
+      }} 
+        title="Add Blog Post"/>
     </View>
   )
 }
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import { Context } from '../context/BlogContext'
+import { SimpleLineIcons } from '@expo/vector-icons'
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context)
@@ -10,8 +11,23 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text> {blogPost.title} </Text>
+      <Text> {blogPost.content} </Text>
     </View>
   )
+}
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Edit', {id: navigation.getParam('id') })
+        }
+      >
+        <SimpleLineIcons name="pencil" size={20}/>
+      </TouchableOpacity>
+    )
+  }
 }
 
 const styles = StyleSheet.create({})
